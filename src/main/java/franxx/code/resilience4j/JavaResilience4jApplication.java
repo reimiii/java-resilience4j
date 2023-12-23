9fed2729d52fcbb78b8b5ef729742180b090a20f
Hilmi AM <imiia75775@proton.me>
2023-12-23 22:11:02 +0700

    Initial commit, add resilience4j all

---
+++ src/main/java/franxx/code/resilience4j/JavaResilience4jApplication.java
@@ -0,0 +1,13 @@
+package franxx.code.resilience4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaResilience4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaResilience4jApplication.class, args);
+	}
+
+}
---
+++ src/test/java/franxx/code/resilience4j/JavaResilience4jApplicationTests.java
@@ -0,0 +1,13 @@
+package franxx.code.resilience4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaResilience4jApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
