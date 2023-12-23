82b76f3b9e14953dd2d0035b0e7d710c8030b26e
Hilmi AM <imiia75775@proton.me>
2023-12-23 22:27:01 +0700

    Retry Lambda, TODO: NEED TO LOOK AT LAMBDA

---
+++ src/test/java/franxx/code/resilience4j/RetryTest.java
@@ -0,0 +1,43 @@
+package franxx.code.resilience4j;
+
+import io.github.resilience4j.retry.Retry;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Supplier;
+
+@Slf4j
+public class RetryTest {
+
+    void callMe() {
+        log.info("Try Call Me");
+        throw new IllegalArgumentException("Ups err");
+
+    }
+
+    @Test
+    void createNewRetry() {
+
+        Retry mee = Retry.ofDefaults("mee");
+
+        Runnable runnable = Retry.decorateRunnable(mee, () -> callMe());
+        runnable.run();
+
+    }
+
+    String helloCall() {
+
+        log.info("Try hello call");
+
+        throw new IllegalArgumentException("Ups err");
+    }
+
+    @Test
+    void retrySupplier() {
+
+        Retry mee = Retry.ofDefaults("mee");
+
+        Supplier<String> stringSupplier = Retry.decorateSupplier(mee, () -> helloCall());
+        stringSupplier.get();
+    }
+}
