1ca651d1d81ada20b272d81ed0d3cd5fb32b699a
Hilmi AM <imiia75775@proton.me>
2023-12-24 11:53:02 +0700

    Rate Limiter config

--- src/test/java/franxx/code/resilience4j/RateLimiterTest.java
+++ src/test/java/franxx/code/resilience4j/RateLimiterTest.java
@@ -1,9 +1,13 @@
 package franxx.code.resilience4j;
 
 import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.ratelimiter.RequestNotPermitted;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Slf4j
@@ -23,5 +27,28 @@ public class RateLimiterTest {
                 log.info("value {}", get);
             }).run();
         }
+    }
+
+    @Test
+    void rateLimitConfig() {
+
+        RateLimiterConfig config = RateLimiterConfig.custom()
+                .limitForPeriod(100)
+                .limitRefreshPeriod(Duration.ofSeconds(30))
+                .timeoutDuration(Duration.ofSeconds(2))
+                .build();
+
+        RateLimiter mee = RateLimiter.of("mee", config);
+
+        Assertions.assertThrows(RequestNotPermitted.class, () -> {
+
+            for (int i = 0; i < 10_000; i++) {
+
+                RateLimiter.decorateRunnable(mee, () -> {
+                    long get = counter.incrementAndGet();
+                    log.info("value {}", get);
+                }).run();
+            }
+        });
     }
 }
