15c47b101306b6b9f68bd80623923329f60f92e1
Hilmi AM <imiia75775@proton.me>
2023-12-26 15:31:49 +0700

    Bulkhead Config

--- src/test/java/franxx/code/resilience4j/BulkHeadTest.java
+++ src/test/java/franxx/code/resilience4j/BulkHeadTest.java
@@ -1,11 +1,14 @@
 package franxx.code.resilience4j;
 
 import io.github.resilience4j.bulkhead.Bulkhead;
+import io.github.resilience4j.bulkhead.BulkheadConfig;
 import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
+import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.function.Supplier;
@@ -18,7 +21,7 @@ public class BulkHeadTest {
     @SneakyThrows
     void slow() {
         log.info("Slow counter {}", counter.incrementAndGet());
-        Thread.sleep(5_000L);
+        Thread.sleep(1_000L);
     }
 
     @Test
@@ -44,5 +47,41 @@ public class BulkHeadTest {
             Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(defaults, () -> slow());
             supplier.get();
         }
+    }
+
+    @Test
+    void semaphoreConfig() throws InterruptedException {
+        BulkheadConfig config = BulkheadConfig.custom()
+                .maxConcurrentCalls(5)
+                .maxWaitDuration(Duration.ofSeconds(2))
+                .build();
+
+
+        Bulkhead mee = Bulkhead.of("mee", config);
+
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = Bulkhead.decorateRunnable(mee, () -> slow());
+            new Thread(runnable).start();
+        }
+
+        Thread.sleep(10_000L);
+    }
+    @Test
+    void threadPoolConfig() throws InterruptedException {
+        ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(3)
+                .coreThreadPoolSize(2)
+                .build();
+
+        log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
+
+        ThreadPoolBulkhead defaults = ThreadPoolBulkhead.of("mee", config);
+
+        for (int i = 0; i < 20; i++) {
+            Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(defaults, () -> slow());
+            supplier.get();
+        }
+
+        Thread.sleep(5000);
     }
 }
