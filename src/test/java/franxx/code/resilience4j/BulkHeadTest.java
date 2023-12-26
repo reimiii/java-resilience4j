0a85068b2f8f111904b272577ac2aabeef838fd3
Hilmi AM <imiia75775@proton.me>
2023-12-26 16:39:35 +0700

    Bulkhead Registry

--- src/test/java/franxx/code/resilience4j/BulkHeadTest.java
+++ src/test/java/franxx/code/resilience4j/BulkHeadTest.java
@@ -1,9 +1,6 @@
 package franxx.code.resilience4j;
 
-import io.github.resilience4j.bulkhead.Bulkhead;
-import io.github.resilience4j.bulkhead.BulkheadConfig;
-import io.github.resilience4j.bulkhead.ThreadPoolBulkhead;
-import io.github.resilience4j.bulkhead.ThreadPoolBulkheadConfig;
+import io.github.resilience4j.bulkhead.*;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
@@ -66,6 +63,7 @@ public class BulkHeadTest {
 
         Thread.sleep(10_000L);
     }
+
     @Test
     void threadPoolConfig() throws InterruptedException {
         ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
@@ -76,6 +74,48 @@ public class BulkHeadTest {
         log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
 
         ThreadPoolBulkhead defaults = ThreadPoolBulkhead.of("mee", config);
+
+        for (int i = 0; i < 20; i++) {
+            Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(defaults, () -> slow());
+            supplier.get();
+        }
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    void semaphoreConfigRegistry() throws InterruptedException {
+        BulkheadConfig config = BulkheadConfig.custom()
+                .maxConcurrentCalls(5)
+                .maxWaitDuration(Duration.ofSeconds(2))
+                .build();
+
+        BulkheadRegistry registry = BulkheadRegistry.ofDefaults();
+        registry.addConfiguration("conf", config);
+
+        Bulkhead mee = registry.bulkhead("mee", "conf");
+
+        for (int i = 0; i < 10; i++) {
+            Runnable runnable = Bulkhead.decorateRunnable(mee, () -> slow());
+            new Thread(runnable).start();
+        }
+
+        Thread.sleep(10_000L);
+    }
+
+    @Test
+    void threadPoolConfigRegistry() throws InterruptedException {
+        ThreadPoolBulkheadConfig config = ThreadPoolBulkheadConfig.custom()
+                .maxThreadPoolSize(3)
+                .coreThreadPoolSize(2)
+                .build();
+
+        ThreadPoolBulkheadRegistry registry = ThreadPoolBulkheadRegistry.ofDefaults();
+        registry.addConfiguration("conf", config);
+
+        log.info(String.valueOf(Runtime.getRuntime().availableProcessors()));
+
+        ThreadPoolBulkhead defaults = registry.bulkhead("mee", "conf");
 
         for (int i = 0; i < 20; i++) {
             Supplier<CompletionStage<Void>> supplier = ThreadPoolBulkhead.decorateRunnable(defaults, () -> slow());
