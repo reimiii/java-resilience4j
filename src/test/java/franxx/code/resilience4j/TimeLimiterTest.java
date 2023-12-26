7e6039eecd6ef19db35aa45bca6cd0c11eb948bb
Hilmi AM <imiia75775@proton.me>
2023-12-26 17:09:59 +0700

    TimeLimiter Config

--- src/test/java/franxx/code/resilience4j/TimeLimiterTest.java
+++ src/test/java/franxx/code/resilience4j/TimeLimiterTest.java
@@ -1,10 +1,12 @@
 package franxx.code.resilience4j;
 
 import io.github.resilience4j.timelimiter.TimeLimiter;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import java.time.Duration;
 import java.util.concurrent.*;
 
 @Slf4j
@@ -26,6 +28,28 @@ public class TimeLimiterTest {
         Future<String> submit = service.submit(() -> slow());
 
         TimeLimiter timeLimiter = TimeLimiter.ofDefaults("mee");
+
+        Callable<String> stringCallable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> submit);
+
+        stringCallable.call();
+
+    }
+
+    @Test
+    void timeLimitConf() throws Exception {
+
+        ExecutorService service = Executors.newSingleThreadExecutor();
+
+        Future<String> submit = service.submit(() -> slow());
+
+        TimeLimiterConfig config = TimeLimiterConfig.custom()
+                .timeoutDuration(Duration.ofSeconds(8))
+                .cancelRunningFuture(true)
+                .build();
+
+
+
+        TimeLimiter timeLimiter = TimeLimiter.of("mee", config);
 
         Callable<String> stringCallable = TimeLimiter.decorateFutureSupplier(timeLimiter, () -> submit);
 
