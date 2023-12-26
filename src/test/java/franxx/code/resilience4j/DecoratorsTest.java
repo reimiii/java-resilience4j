ab44118e080bd8fc1ae9ab175e0c56e4badcfd09
Hilmi AM <imiia75775@proton.me>
2023-12-27 01:27:49 +0700

    Decorators

---
+++ src/test/java/franxx/code/resilience4j/DecoratorsTest.java
@@ -0,0 +1,49 @@
+package franxx.code.resilience4j;
+
+import io.github.resilience4j.decorators.Decorators;
+import io.github.resilience4j.ratelimiter.RateLimiter;
+import io.github.resilience4j.ratelimiter.RateLimiterConfig;
+import io.github.resilience4j.retry.Retry;
+import io.github.resilience4j.retry.RetryConfig;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+@Slf4j
+public class DecoratorsTest {
+
+    @SneakyThrows
+    public void slow() {
+        log.info("Slow");
+        Thread.sleep(1000);
+        throw new IllegalArgumentException("Err");
+    }
+
+
+    @Test
+    void decorators() throws InterruptedException {
+
+        RateLimiter rateLimiter = RateLimiter.of("mee-rate", RateLimiterConfig.custom()
+                .limitForPeriod(5)
+                .limitRefreshPeriod(Duration.ofMinutes(1))
+                .build());
+
+        Retry retry = Retry.of("mee-ret", RetryConfig.custom()
+                .maxAttempts(10)
+                .waitDuration(Duration.ofMillis(10))
+                .build());
+
+        Runnable runnable = Decorators.ofRunnable(() -> slow())
+                .withRetry(retry)
+                .withRateLimiter(rateLimiter)
+                .decorate();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(runnable).start();
+        }
+
+        Thread.sleep(10_000L);
+    }
+}
