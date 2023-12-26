c20d6bb7cc908ee28bb5f70d0a37d50a822940c9
Hilmi AM <imiia75775@proton.me>
2023-12-26 22:38:03 +0700

    Circuit Breaker Config

--- src/test/java/franxx/code/resilience4j/CircuitBreakerTest.java
+++ src/test/java/franxx/code/resilience4j/CircuitBreakerTest.java
@@ -1,6 +1,7 @@
 package franxx.code.resilience4j;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +19,30 @@ public class CircuitBreakerTest {
     void circuitBreaker() {
 
         CircuitBreaker mee = CircuitBreaker.ofDefaults("mee");
+
+        for (int i = 0; i < 200; i++) {
+            try {
+                int finalI = i;
+                CircuitBreaker
+                        .decorateRunnable(mee, () -> err(finalI))
+                        .run();
+            } catch (Exception e) {
+                log.error("Error is {}", e.getMessage());
+            }
+        }
+    }
+
+    @Test
+    void circuitBreakerConf() {
+
+        CircuitBreakerConfig build = CircuitBreakerConfig.custom()
+                .slidingWindowType(CircuitBreakerConfig.SlidingWindowType.COUNT_BASED)
+                .failureRateThreshold(10f)
+                .slidingWindowSize(10)
+                .minimumNumberOfCalls(10)
+                .build();
+
+        CircuitBreaker mee = CircuitBreaker.of("mee", build);
 
         for (int i = 0; i < 200; i++) {
             try {
